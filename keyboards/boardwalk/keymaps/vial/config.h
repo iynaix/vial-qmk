57c27912831b8a09d30076b411f075fe51b0974d
Lin Xianyi <iynaix@gmail.com>
2024-03-06 12:04:27 +0800

    Add support for boardwalk

---
+++ keyboards/boardwalk/keymaps/vial/config.h
@@ -0,0 +1,39 @@
+/* Copyright 2021 Gigahawk
+ * Modified 2021 by usrfriendly for vial-qmk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+# pragma once
+
+#define VIAL_KEYBOARD_UID {0xEA, 0xE5, 0x60, 0x25, 0x26, 0xB1, 0x64, 0xE5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 13 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 2 }
+
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+#define NO_ACTION_ONESHOT
+
+#undef RGBLIGHT_ANIMATIONS
+#undef RGBLIGHT_EFFECT_BREATHING
+#undef RGBLIGHT_EFFECT_RAINBOW_MOOD
+#undef RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#undef RGBLIGHT_EFFECT_SNAKE
+#undef RGBLIGHT_EFFECT_KNIGHT
+#undef RGBLIGHT_EFFECT_CHRISTMAS
+#undef RGBLIGHT_EFFECT_STATIC_GRADIENT
+#undef RGBLIGHT_EFFECT_RGB_TEST
+#undef RGBLIGHT_EFFECT_ALTERNATING
+#undef RGBLIGHT_EFFECT_TWINKLE
