720559c5aa4e9f7a6c27efdcec34033e3db8873f
Lin Xianyi <iynaix@gmail.com>
2024-03-06 11:54:11 +0800

    Add support for jelly evolv

---
+++ keyboards/owlab/jelly_evolv/hotswap/625u/keymaps/vial/config.h
@@ -0,0 +1,26 @@
+/* Copyright 2021 Gigahawk
+ * Modified 2021 by usrfriendly for vial-qmk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+# pragma once
+
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+#define RGB_MATRIX_LED_COUNT 16
+
+#define VIAL_KEYBOARD_UID {0xD4, 0xC8, 0x92, 0x82, 0x06, 0xA8, 0x42, 0x33}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 11 }
---
+++ keyboards/owlab/jelly_evolv/hotswap/625u/keymaps/vial/rules.mk
@@ -0,0 +1,4 @@
+LTO_ENABLE = yes
+RGBLIGHT_ENABLE = yes
+VIAL_ENABLE = yes
+VIA_ENABLE = yes
